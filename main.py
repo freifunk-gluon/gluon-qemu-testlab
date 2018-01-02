6085da1b6c4e146bcd5e6118f7cb0bb8b2471a7f
lemoer <git@irrelefant.net>
2018-01-02 03:48:20 +0100

    use tmux + ssh + network namespaces

--- main.py
+++ main.py
@@ -11,6 +11,18 @@ import subprocess
 
 image = "image.img"
 
+if os.environ.get('TMUX') is None:
+    os.execl('/usr/bin/tmux', 'tmux', '-S', 'test', 'new', sys.executable, '-i', *sys.argv)
+
+SSH_KEY_FILE = './ssh/id_rsa.key'
+SSH_PUBKEY_FILE = SSH_KEY_FILE + '.pub'
+
+def run(cmd):
+    subprocess.run(cmd, shell=True)
+
+# TODO: cd to project folder
+if not os.path.exists(SSH_PUBKEY_FILE):
+    run(f'ssh-keygen -t rsa -f {SSH_KEY_FILE} -N \'\'')
 
 def gen_qemu_call(image, identifier, ports):
 
@@ -20,6 +32,7 @@ def gen_qemu_call(image, identifier, ports):
     host_id = 1
     nat_mac = "52:54:%02x:%02x:34:%02x" % (host_id, identifier, 1)
     client_mac = "52:54:%02x:%02x:34:%02x" % (host_id, identifier, 2)
+    hostname = 'node%d' % identifier
 
     mesh_ifaces = []
     mesh_id = 1
@@ -41,7 +54,7 @@ def gen_qemu_call(image, identifier, ports):
     call = ['-nographic',
             '-netdev', 'user,id=hn1',
             '-device', 'e1000,addr=0x06,netdev=hn1,id=nic1,mac=' + nat_mac,
-            '-netdev', 'tap,id=hn2,script=no,downscript=no',
+            '-netdev', 'tap,id=hn2,script=no,downscript=no,ifname=%s_client' % hostname,
             '-device', 'e1000,addr=0x05,netdev=hn2,id=nic2,mac=' + client_mac]
 
     process = subprocess.Popen(['qemu-system-x86_64', './images/%02x.img' % identifier] + call + mesh_ifaces, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -65,8 +78,36 @@ def set_mesh_devs(p, devs):
         call(p, 'ethtool -K %s gso off' % d)
 
     call(p, 'uci commit network')
-    call(p, '/etc/init.d/network restart')
+    call(p, 'ubus call network reload')
 
+def add_ssh_key(p):
+    # TODO: this removes baked in ssh keys :/
+    with open(SSH_PUBKEY_FILE) as f:
+        content = f.read()
+        call(p, f'''cat >> /etc/dropbear/authorized_keys <<EOF
+{content}
+EOF''')
+
+
+def install_clients(hostnames):
+    for hostname in hostnames:
+        ifname = "%s_client" % hostname
+        netns = "%s_client" % hostname
+        # TODO: delete them correctly
+        # Issue with mountpoints yet http://man7.org/linux/man-pages/man7/mount_namespaces.7.html
+        run(f'ip netns add {netns}')
+        run(f'sudo ip link set netns {netns} dev {ifname}')
+        run(f'ip netns exec {netns} ip link set {ifname} up')
+        shell = os.environ.get('SHELL') or '/bin/bash'
+        spawn_in_tmux(hostname.replace('node', 'client'), f'ip netns exec {netns} {shell}')
+
+        ssh_opts = '-o UserKnownHostsFile=/dev/null ' + \
+                   '-o StrictHostKeyChecking=no ' + \
+                   f'-i {SSH_KEY_FILE} '
+        spawn_in_tmux(hostname, f'ip netns exec {netns} /bin/bash -c "while ! ssh {ssh_opts} root@fdca:ffee:8::1; do sleep 1; done"')
+
+def spawn_in_tmux(title, cmd):
+    run(f'tmux -S test new-window -n {title} {cmd}')
 
 p = gen_qemu_call(image, 1, {1234: 'listen'})
 time.sleep(5)
@@ -74,7 +115,9 @@ p2 = gen_qemu_call(image, 2, {1234: 'connect', 1235: 'listen'})
 time.sleep(5)
 p3 = gen_qemu_call(image, 3, {1235: 'connect' })
 
-time.sleep(150)
+for i in range(15):
+    print(i*10)
+    time.sleep(10)
 
 # activate shells
 call(p, '')
@@ -100,11 +143,49 @@ call(p, 'pretty-hostname node1')
 call(p2, 'pretty-hostname node2')
 call(p3, 'pretty-hostname node3')
 
+add_ssh_key(p)
+add_ssh_key(p2)
+add_ssh_key(p3)
+
+
 def read_all(p):
     res = b""
     while select.select([p.stdout],[],[],0)[0] != []:
-        res += p.stdout.read(1)
+        res += os.read(p.stdout.fileno(), 1)
     return res
+
+call(p, "echo -n 'sucessfully '; echo 'configured'")
+call(p2, "echo -n 'sucessfully '; echo 'configured'")
+call(p3, "echo -n 'sucessfully '; echo 'configured'")
+
+print('waiting to configure')
+
+with open('logs/node1.log', 'wb') as f1:
+    with open('logs/node2.log', 'wb') as f2:
+        with open('logs/node3.log', 'wb') as f3:
+            node1_log = b""
+            node2_log = b""
+            node3_log = b""
+            while True:
+                c1 = read_all(p)
+                f1.write(c1)
+                node1_log += c1
+
+                c2 = read_all(p2)
+                f2.write(c2)
+                node2_log += c2
+
+                c3 = read_all(p3)
+                f3.write(c3)
+                node3_log += c3
+
+                if b"sucessfully configured" in node1_log \
+                    and b"sucessfully configured" in node2_log \
+                    and b"sucessfully configured" in node3_log:
+                    break
+
+install_clients(['node1', 'node2', 'node3'])
+
 
 def enable_echo(enable):
     fd = sys.stdin.fileno()
@@ -117,7 +198,6 @@ def enable_echo(enable):
         new[3] &= ~termios.ECHO # no echo
         new[3] &= ~termios.ICANON # Input is delivered bytewise instead of linewise
         new[3] &= ~termios.ISIG # Disable Signals on CTRL + C
-
     termios.tcsetattr(fd, termios.TCSANOW, new)
 
 
