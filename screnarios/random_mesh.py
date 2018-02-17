3a19d01e886f6e302b1f7539f1edda156a21eda9
lemoer <git@irrelefant.net>
2018-02-17 18:25:09 +0100

    add random_mesh scenario and image of random mesh

---
+++ screnarios/random_mesh.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python36
+import sys
+import random
+sys.path.append(".")
+from pynet import *
+
+NODE_COUNT = 30
+
+nodes = []
+
+for i in range(NODE_COUNT):
+    n = Node()
+
+    if len(nodes) > 0:
+        connect(n, random.choice(nodes))
+
+    nodes.append(n)
+
+run_all()
